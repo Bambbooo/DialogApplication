9a26531d322f1df751e52ab1f84f8cfb5fbbce3f
Bambo <balllint999@gmail.com>
2018-10-08 17:12:28 +0200

    test

--- app/src/main/java/com/bambo/dialogapplication/MainActivity.java
+++ app/src/main/java/com/bambo/dialogapplication/MainActivity.java
@@ -1,14 +1,19 @@
 package com.bambo.dialogapplication;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         /* changes */
+        Log.d(TAG, "onCreate: test");
     }
 }
